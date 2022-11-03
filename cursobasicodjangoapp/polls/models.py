783dc79d5baef2d229ba3ee44c80b3bcc68443eb
Jesus <bep010795@gmail.com>
2022-11-02 22:48:45 -0600

    Creacion de primeras vistas y plantillas

--- cursobasicodjangoapp/polls/models.py
+++ cursobasicodjangoapp/polls/models.py
@@ -1,13 +1,22 @@
-from email.policy import default
-from unittest.util import _MAX_LENGTH
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length = 200)
     pub_date = models.DateTimeField("Date of published")
 
+    def __str__(self):
+        return self.question_text
+
+    def was_recently_published(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length = 200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
--- cursobasicodjangoapp/polls/views.py
+++ cursobasicodjangoapp/polls/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
-# Create your views here.
+
 def index(request):
-    return HttpResponse("Hello world")
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        "latest_question_list": latest_question_list
+    })
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Estas viendo la pregunta numero {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta numero {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Estas voatndo a la pregunta numero {question_id}")
